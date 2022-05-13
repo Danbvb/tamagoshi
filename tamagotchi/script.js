297cb136d1e2d3e72909af9ea5ccc4d7b141e0d7
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-05-13 18:14:16 -0300

    modificacoes

--- tamagotchi/script.js
+++ tamagotchi/script.js
@@ -6,57 +6,60 @@ var social = 100;
 var idade = 0;
 
 function Loop(time = 2) {
-  fome = fome - parseInt(time) * 0.5;
+  var total = fome + higiene + sono + diversao + social;
+
+  fome = fome - parseInt(time) * 1;
   higiene = higiene - parseInt(time) * 0.25;
   sono = sono - parseInt(time) * 1;
   diversao = diversao - parseInt(time) * 0.25;
   social = social - parseInt(time) * 0.3;
   idade = idade + parseInt(time) * 0.5;
 
-  document.getElementById("fome").innerHTML = fome + "%";
+  document.getElementById("fome").innerHTML = fome.toFixed(2) + "%";
   document.getElementById("higiene").innerHTML = higiene.toFixed(1) + "%";
   document.getElementById("sono").innerHTML = sono.toFixed(1) + "%";
   document.getElementById("diversao").innerHTML = diversao.toFixed(1) + "%";
   document.getElementById("social").innerHTML = social.toFixed(1) + "%";
   document.getElementById("idade").innerHTML = idade;
 
-  if (idade >= 5) {
-    document.getElementById("pet").src = "img/rodrigo.png";
-  } else if (idade >= 15) {
-    document.getElementById("pet").src = "./img/oldrigo.jpeg";
-  }
-
   if (
-    fome == 0 ||
-    higiene == 0 ||
-    sono == 0 ||
-    diversao == 0 ||
-    social == 0 ||
-    idade >= 80
+    fome <= 0 ||
+    higiene <= 0 ||
+    sono <= 0 ||
+    diversao <= 0 ||
+    social <= 0 ||
+    idade >= 50
   ) {
-    alert("SEU BONEQUINHO MORREU.");
-    document.location.reload(true);
+    document.getElementById("pet").src = "img/preto.jpg";
+    // alert("SEU BONEQUINHO MORREU.");
+    // document.location.reload(true);
+  } else if (total > 400) {
+    document.getElementById("pet").src = "img/azul.jpg";
+  } else if (total > 300) {
+    document.getElementById("pet").src = "img/amarelo.jpg";
+  } else if (total > 200) {
+    document.getElementById("pet").src = "img/vermelho.jpg";
   }
 }
 
 function Comer() {
-  fome = fome + 10;
+  fome += 4;
 }
 
 function Banho() {
-  higiene = higiene + 10;
+  higiene += 4;
 }
 
 function Dormir() {
-  sono = sono + 10;
+  sono += 3;
 }
 
 function Brincar() {
-  diversao = diversao + 5;
+  diversao += 2;
 }
 
 function Conversar() {
-  social = social + 5;
+  social += 2;
 }
 
 function Start() {
